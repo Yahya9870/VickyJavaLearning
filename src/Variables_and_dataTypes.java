3ef0b488f069caba138a8c9ab5bb5e105225cdbf
Yahya9870 <56015489+Yahya9870@users.noreply.github.com>
2025-01-15 09:13:49 -0500

    first commit

---
+++ src/Variables_and_dataTypes.java
@@ -0,0 +1,54 @@
+public class Variables_and_dataTypes {
+    public static void main(String[] args) {
+        // In a broader term there are 2 kinds of dataTypes in java.
+
+        //1> Primitive (byte, short, int, long, float, double, char and boolean)
+        //2. Non-Primitive (Stacks, Arrays, LinkedLists etc.)
+
+
+        // Integers (byte, short and int)
+
+        // byte 1b memory   -128 to 127
+        // short 2b memory
+        // int 4b
+        // long 8b
+
+        // in software engineering its not always about getting the right solutions,
+        // its about getting the optimal one, using minimal time and space.
+
+        // int a = 10; 10 is the literal
+
+        // Decimal value (float and double)
+
+        // the decimal literals in java is double bydefault
+
+       // float f =  6.7f;
+
+       // double d =  6.7f;
+
+        // Character char
+
+        char a='a';
+        // ASCII Values
+        // in programming languages every alphabet and special characters like $ are associated with an integer value
+
+        int b=a; // no error
+        System.out.println("The ASCII value of a is "+b);
+
+//        int x =100;
+//        System.out.println("x");
+
+
+        // Boolean dataType (True or False)
+
+        boolean b1= true;
+
+        System.out.println(b1);
+
+
+
+
+
+    }
+
+}
